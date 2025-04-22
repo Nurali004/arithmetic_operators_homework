095263d7abb0967f4efd3afe511cbb36834d167c
M A C W I N <mavzurovnurali@gmail.com>
2025-04-22 18:52:25 +0500

    oxirgi misol yechimi

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,12 @@
 #Create a variable called 'number' and assign any value of int to it.
+number=333
  
 #Divide the number by 2 and assign number.
  
 #Find the fraction of the number and assign to a variable called 'answer'.
+answer=number/2
  
 #Print the answer.
+print(answer)
  
 #Print the number.
